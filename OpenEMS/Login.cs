eefca05af9df055a13e4a17cdbeac7d50ae0363b
Alex Dasneves <adasneves127@gmail.com>
2025-01-13 20:17:42 -0500

    Added Regkey Saving of DB Info

--- OpenEMS/Login.cs
+++ OpenEMS/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using Microsoft.Win32;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,12 @@ namespace OpenEMS
         private void btnLogin_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
+
+            RegistryKey newKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\OpenEMS");
+            newKey.SetValue("db_name", txtDBName.Text);
+            newKey.SetValue("db_host", txtFQDN.Text);
+            newKey.SetValue("db_user", txtUsername.Text);
+
             this.Close();
         }
 
@@ -27,6 +34,28 @@ namespace OpenEMS
         {
             this.DialogResult = DialogResult.Cancel;
             this.Close();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\OpenEMS"))
+            {
+                if (key != null)
+                {
+                    txtDBName.Text = key.GetValue("db_name").ToString();
+                    txtFQDN.Text = (string)key.GetValue("db_host");
+                    txtUsername.Text = (string)key.GetValue("db_user");
+                    txtPassword.Focus();
+                }
+            }
+        }
+
+        private void txtUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                btnLogin_Click(null, null);
+            }
         }
     }
 }
